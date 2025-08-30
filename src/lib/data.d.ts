5483d8a5f31e8da5746759f8265869d9c9f6fd82
Aftaza <taftazani17@student.ub.ac.id>
2025-08-30 07:32:42 +0700

    feat: update profile card animation and style

---
+++ src/lib/data.d.ts
@@ -0,0 +1,10 @@
+export interface ProfileCardProps {
+    image?: string;
+    title?: string;
+    subtitle?: string;
+    author?: string;
+    year?: string;
+    category?: string;
+    description?: string;
+    tags?: string[];
+}
